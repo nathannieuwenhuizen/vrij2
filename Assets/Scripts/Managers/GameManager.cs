2a330b5daa306cf4fd0e151cfb6bbb99b2cc08a6
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-05-11 17:04:46 +0200

    pause menu added

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject pauseScreen;
+    [SerializeField]
+    private GameObject pauseButton;
+    [SerializeField]
+    private FadeImage fadeImage;
     void Start()
     {
         AudioManager.instance?.Playmusic(Music.museum, .5f * Settings.Music);
     }
+
+
+    public void Pause(bool val)
+    {
+        //AudioManager.instance?.PlaySound(AudioEffect) //TODO: button click
+        AudioManager.instance?.CHangeMusicVolume(val ? .2f : 1f); // change music volume
+
+        fadeImage.alpha = val ? .5f : 0f;
+        Time.timeScale = val ? 0 : 1f;
+        pauseButton.SetActive(!val);
+        pauseScreen.SetActive(val);
+    }
+
+
 
 }
---
+++ Assets/Scripts/UI/FadeImage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour
+{
+
+    private Image img;
+    private Color color;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    public float alpha
+    {
+        get
+        {
+            return img.color.a;
+        }
+        set
+        {
+            img.color = new Color(img.color.r, img.color.g, img.color.b, value);
+        }
+    }
+
+    public IEnumerator FadeTo(float _startAlpha, float _endAlpha, float _duration)
+    {
+        StopAllCoroutines();
+
+
+        color = img.color;
+
+
+        float startAlpha = _startAlpha;
+        float index = 0;
+
+        while (index < _duration)
+        {
+            index += Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+            color.a = Mathf.Lerp(startAlpha, _endAlpha, index);
+            img.color = color;
+        }
+        color.a = _endAlpha;
+        img.color = color;
+    }
+
+}
