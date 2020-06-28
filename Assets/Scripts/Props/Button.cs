40f665fcc0e19d24e06fa3e65263f368b7ba75a0
Nathan Nieuwenhuizen <nathan-san@live.nl>
2020-06-28 18:34:57 +0200

    ui button animation when hover

--- Assets/Scripts/Props/Button.cs
+++ Assets/Scripts/Props/Button.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(LineRenderer))]
-public class Button : InteractableObject
+public class WallButton : InteractableObject
 {
     [SerializeField]
     private UnityEvent pushEvent;
---
+++ Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private bool hover = false;
+
+    private RectTransform rt;
+    private Vector3 min = Vector3.one;
+    private Vector3 max = Vector3.one * 1.1f;
+    private float sizeSpeed = 5f;
+    private void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+    private void Update()
+    {
+        rt.localScale = Vector3.Lerp(rt.localScale, hover ? max : min , Time.deltaTime * sizeSpeed);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hover = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hover = false;
+    }
+}
